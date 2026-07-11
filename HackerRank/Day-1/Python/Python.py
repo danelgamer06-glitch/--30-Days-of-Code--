bcb9397f6dce48ad5abc1be92199bff709f0c229
danelgamer06-glitch <danelgamer06@gmail.com>
2026-07-11 12:13:49 -0400

    Revise input prompts and string message in Python.py
    Updated prompts for user input and modified the string message.

--- HackerRank/Day-1/Python/Python.py
+++ HackerRank/Day-1/Python/Python.py
@@ -1,15 +1,15 @@
 i = 4
 d = 4.0
-s = 'HackerRank'
+s = 'Nitido completaste el primer dia de 30 dias de codigo en Python. '
 # Read and save an integer, double, and String to your variables.
 entero = 0 
 decimal = 0.0
 cadena_texto = ""
 # Print the sum of both integer variables on a new line.
 
-entero = int(input())
-decimal = float(input())
-cadena_texto = input()
+entero = int(input("Ingresa un número entero: "))
+decimal = float(input("Ingresa un número decimal: "))
+cadena_texto = input("Ingresa una cadena de texto: ")
 
 # Print the sum of the double variables on a new line.
 print(i + entero)
@@ -18,4 +18,5 @@ print(d + decimal)
 # Concatenate and print the String variables on a new line
 # The 's' variable above should be printed first.
 print(f"{s + cadena_texto}")
+
 
