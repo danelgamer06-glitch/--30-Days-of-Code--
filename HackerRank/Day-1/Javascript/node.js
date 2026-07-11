fad4bcaf36ab7985147718eca130709fbe710f9e
danelgamer06-glitch <danelgamer06@gmail.com>
2026-07-11 12:25:42 -0400

    Update node.js

--- HackerRank/Day-1/Javascript/node.js
+++ HackerRank/Day-1/Javascript/node.js
@@ -1,19 +1,3 @@
-process.stdin.resume();
-process.stdin.setEncoding('ascii');
-
-var input_stdin = "";
-var input_stdin_array = "";
-var input_currentline = 0;
-
-process.stdin.on('data', function (data) {
-    input_stdin += data;
-});
-
-process.stdin.on('end', function () {
-    input_stdin_array = input_stdin.split("\n");
-    main();    
-});
-
 // Reads complete line from STDIN
 function readLine() {
     return input_stdin_array[input_currentline++];
@@ -22,19 +6,19 @@ function readLine() {
 function main() {
     var i = 4
     var d = 4.0
-    var s = "HackerRank "
+    var s = "Bien entrado a 30 días de código! "
  
     // 1. Declarar y leer las variables de entrada
-    var i2 = parseInt(readLine());
-    var d2 = parseFloat(readLine());
-    var s2 = readLine();
+    var entero = parseInt(readLine("Ingresa un número entero: "));
+    var decimal = parseFloat(readLine("Ingresa un número decimal: "));
+    var cadena_texto = readLine("Ingresa una cadena de texto: ");
 
     // 2. Imprimir la suma de ambos enteros
-    console.log(i + i2);
+    console.log(i + entero);
 
     // 3. Imprimir la suma de los dobles con un decimal
-    console.log((d + d2).toFixed(1));
+    console.log((d + decimal).toFixed(1));
 
     // 4. Concatenar e imprimir las variables de tipo String
-    console.log(s + s2);
+    console.log(s + cadena_texto);
 }
