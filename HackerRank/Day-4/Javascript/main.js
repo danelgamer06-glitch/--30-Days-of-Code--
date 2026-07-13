eb01f2cf37bfe8dfeacfc254c7ccd9c6df66404a
danelgamer06-glitch <danelgamer06@gmail.com>
2026-07-12 23:32:36 -0400

    Update main.js

--- HackerRank/Day-4/Javascript/main.js
+++ HackerRank/Day-4/Javascript/main.js
@@ -19,16 +19,16 @@ function readLine() {
 }
 
 function Person(initialAge){
-    // Paso 1: Validar y asignar la edad
+    // Check if initialAge is valid
     if (initialAge < 0) {
         console.log("Age is not valid, setting age to 0.");
         this.age = 0;
     } else {
         this.age = initialAge;
     }
-
-    // Paso 2: Determinar la etapa de vida
+    
     this.amIOld = function(){
+        // Do some computations in here and print out the correct statement
         if (this.age < 13) {
             console.log("You are young.");
         } else if (this.age >= 13 && this.age < 18) {
@@ -37,25 +37,22 @@ function Person(initialAge){
             console.log("You are old.");
         }
     };
-
-    // Paso 3: Incrementar la edad
+    
     this.yearPasses = function(){
+        // Increment the age of the person in here
         this.age += 1;
     };
 }
 
 function main() {
-
-var T=parseInt(readLine());
-for(i=0;i<T;i++){
-    var age=parseInt(readLine());
-    var p=new Person(age);
-    p.amIOld();
-    for(j=0;j<3;j++){
-        p.yearPasses();
-        
-    }
-    p.amIOld();
-    console.log("");   
+    var T = parseInt(readLine());
+    for(var i = 0; i < T; i++){
+        var age = parseInt(readLine());
+        var p = new Person(age);
+        p.amIOld();
+        for(var j = 0; j < 3; j++){
+            p.yearPasses();
+        }
+        p.amIOld();
     }
 }
